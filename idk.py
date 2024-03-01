f21aa040330033855a9c15d29d8045a59e8ea23c
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2024-03-01 13:04:42 +0100

    ouahwd

--- idk.py
+++ idk.py
@@ -1,1 +1,82 @@
+import random
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+array_plot = []
+data_plot = []
 
+
+with open("Diabetes_prediction.csv", newline='') as csvfile:
+    data = csv.reader(csvfile)
+    for n, row in enumerate(data):
+        if n != 0:
+            if n == 3:
+                print(row)
+            data_plot.append([float(a) for a in row])
+
+
+def distance(a, b):
+    length_data = [(c - b[d]) ** 2 for d, c in enumerate(a) if d != len(a) - 1]
+    return np.sqrt(np.sum(length_data))
+
+
+def one_nn(p, closest=None):
+    if closest is None:
+        closest = [2093812093810293, "ok"]
+    for a in data_plot:
+        length = distance(a, p)
+        if length < closest[0]:
+            closest = [length, a[-1]]
+    return closest[1]
+
+
+def check_common_number(arr):
+    longest = [len(arr[0]), len(arr[1])]
+    return 1 if longest[0]/longest[1] > 306/694 else 0
+
+
+def sort_similar_numbers(arr, numbers=None, index=None):
+    if index is None:
+        index = []
+    if numbers is None:
+        numbers = []
+    for j, i in enumerate(arr):
+        if j == 0:
+            numbers.append([i[1]])
+            index.append(i[1])
+        else:
+            in_index = [False, 0]
+            for h, g in enumerate(index):
+                if i[1] != g:
+                    in_index = [False, h]
+                else:
+                    in_index = [True, h]
+                    break
+            if in_index[0] is True:
+                numbers[in_index[1]].append(i[1])
+            else:
+                numbers.append([i[1]])
+                index.append(i[1])
+    return check_common_number(numbers)
+
+
+def k_nn(knn_array, p, m, lengths=None):
+    if lengths is None:
+        lengths = []
+    if m % 2 == 0:
+        m = m + 1
+    if len(knn_array) == 1:
+        return one_nn(p)
+    else:
+        for a in knn_array:
+            lengths.append([distance(a, p), a[-1]])
+        lengths.sort()
+        lengths = lengths[:m]
+        number = sort_similar_numbers(lengths)
+        p.append(number)
+        return p
+
+
+point = k_nn(data_plot, [1, 88.14, 63, 23, 149, 22, 0.67, 48.24], 50)
+print(point)
